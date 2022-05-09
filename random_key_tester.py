316ea2b24adfe91e8a1a669ec702db0faa4130da
Mark Lattimore <mark.d.lattimore@gmail.com>
2022-05-08 21:55:20 -0400

    Visual changes to random_key_tester. Adds rich dependency.

--- random_key_tester.py
+++ random_key_tester.py
@@ -2,6 +2,9 @@ from base64 import b64encode
 import os
 import time
 import sys
+from rich.console import Console
+
+console = Console()
 
 
 def generate_key(size):
@@ -14,27 +17,32 @@ key_size = 6
 final = []
 keys = []
 keys_set = set()
-start = time.perf_counter()
-while True:
-    key = generate_key(key_size)
-    keys.append(key)
-    keys_set.add(key)
-    if len(keys) != len(keys_set):  # compare lengths of list and set and break
-        # to determine when duplicate occurs. Once duplicate occurs, list and set
-        # no longer of equal length since set does not allow duplicate values
-        final.append(key)
-        break
-    #if keys.count(key) > 1: THIS IS A VERY SLOW METHOD
-        #final.append(key)
-        #break
-    else:
-        continue
-stop = time.perf_counter()
+print()
+with console.status("Working ... \n"):
+    print()
+    start = time.perf_counter()
+    while True:
+        key = generate_key(key_size)
+        keys.append(key)
+        keys_set.add(key)
+        if len(keys) != len(keys_set):  # compare lengths of list and set and break
+            # to determine when duplicate occurs. Once duplicate occurs, list and set
+            # no longer of equal length since set does not allow duplicate values
+            final.append(key)
+            break
+        #if keys.count(key) > 1: THIS IS A VERY SLOW METHOD
+            #final.append(key)
+            #break
+        else:
+            continue
+    stop = time.perf_counter()
 
 #print(keys) USE ONLY FOR TESTING
+print()
 print(f"The {len(keys)} st/nd/th key was a duplicate")
 print(f"You produced {len(keys_set)} unique keys.")
-print(f"The duplicated key was {final[0]}.")
+print(f"The duplicated key was {final[0]}")
 print(f"Time: {stop - start} seconds")
+print()
     
 
